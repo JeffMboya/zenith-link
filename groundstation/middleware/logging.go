28c621d934a4daffe86821a8468563d14fde1c39
Jeff Mboya <mboyajeff88@gmail.com>
2026-04-27 10:44:47 +0300

    groundstation/middleware: structured request logging with slog

---
+++ groundstation/middleware/logging.go
@@ -0,0 +1,66 @@
+// Package middleware provides service decorators for the groundstation service.
+package middleware
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/absmach/zenith-link/groundstation"
+	"github.com/absmach/zenith-link/pkg/zenith"
+)
+
+var _ groundstation.Service = (*loggingMiddleware)(nil)
+
+type loggingMiddleware struct {
+	logger *slog.Logger
+	svc    groundstation.Service
+}
+
+// NewLogging wraps svc with structured JSON logging on every method call.
+func NewLogging(svc groundstation.Service, logger *slog.Logger) groundstation.Service {
+	return &loggingMiddleware{logger: logger, svc: svc}
+}
+
+func (lm *loggingMiddleware) Receive(ctx context.Context, rawFrame []byte) (tm zenith.Telemetry, err error) {
+	defer func(begin time.Time) {
+		args := []any{
+			slog.String("duration", time.Since(begin).String()),
+			slog.Int("frame_bytes", len(rawFrame)),
+		}
+		if err != nil {
+			args = append(args, slog.Any("error", err))
+			lm.logger.Warn("groundstation.Receive", args...)
+			return
+		}
+		args = append(args, slog.Uint64("sequence", uint64(tm.Sequence)))
+		lm.logger.Info("groundstation.Receive", args...)
+	}(time.Now())
+	return lm.svc.Receive(ctx, rawFrame)
+}
+
+func (lm *loggingMiddleware) Latest(ctx context.Context) (st groundstation.LatestState, err error) {
+	defer func(begin time.Time) {
+		args := []any{slog.String("duration", time.Since(begin).String())}
+		if err != nil {
+			args = append(args, slog.Any("error", err))
+			lm.logger.Warn("groundstation.Latest", args...)
+			return
+		}
+		lm.logger.Debug("groundstation.Latest", args...)
+	}(time.Now())
+	return lm.svc.Latest(ctx)
+}
+
+func (lm *loggingMiddleware) Subscribe(ctx context.Context) (ch <-chan zenith.Telemetry, err error) {
+	defer func(begin time.Time) {
+		args := []any{slog.String("duration", time.Since(begin).String())}
+		if err != nil {
+			args = append(args, slog.Any("error", err))
+			lm.logger.Warn("groundstation.Subscribe", args...)
+			return
+		}
+		lm.logger.Debug("groundstation.Subscribe", args...)
+	}(time.Now())
+	return lm.svc.Subscribe(ctx)
+}
