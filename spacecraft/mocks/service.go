ce564fa5bc28f8caf7885eb3f4044d01b9e5d03c
Jeff Mboya <mboyajeff88@gmail.com>
2026-04-27 10:44:47 +0300

    spacecraft/mocks: generate Service mock for HTTP handler tests

---
+++ spacecraft/mocks/service.go
@@ -0,0 +1,63 @@
+package mocks
+
+import (
+	"context"
+	"time"
+
+	"github.com/absmach/zenith-link/pkg/orbital"
+	"github.com/absmach/zenith-link/pkg/zenith"
+	"github.com/absmach/zenith-link/spacecraft"
+	"github.com/stretchr/testify/mock"
+)
+
+var _ spacecraft.Service = (*Service)(nil)
+
+// Service is a mock of the spacecraft Service interface.
+type Service struct {
+	mock.Mock
+}
+
+func (m *Service) Telemetry(ctx context.Context, t time.Time) (zenith.Telemetry, error) {
+	args := m.Called(ctx, t)
+	return args.Get(0).(zenith.Telemetry), args.Error(1)
+}
+
+func (m *Service) TelemetryFrame(ctx context.Context, t time.Time) ([]byte, error) {
+	args := m.Called(ctx, t)
+	if b, ok := args.Get(0).([]byte); ok {
+		return b, args.Error(1)
+	}
+	return nil, args.Error(1)
+}
+
+func (m *Service) TMFrame(ctx context.Context, t time.Time, frameSize int) ([]byte, error) {
+	args := m.Called(ctx, t, frameSize)
+	if b, ok := args.Get(0).([]byte); ok {
+		return b, args.Error(1)
+	}
+	return nil, args.Error(1)
+}
+
+func (m *Service) State(ctx context.Context, t time.Time) (spacecraft.State, error) {
+	args := m.Called(ctx, t)
+	if st, ok := args.Get(0).(spacecraft.State); ok {
+		return st, args.Error(1)
+	}
+	return spacecraft.State{}, args.Error(1)
+}
+
+func (m *Service) ExecuteCommand(ctx context.Context, tcFrame []byte) (spacecraft.CommandResult, error) {
+	args := m.Called(ctx, tcFrame)
+	if cr, ok := args.Get(0).(spacecraft.CommandResult); ok {
+		return cr, args.Error(1)
+	}
+	return spacecraft.CommandResult{}, args.Error(1)
+}
+
+func (m *Service) Windows(ctx context.Context, gsLat, gsLon float64, start, end time.Time, minElevDeg float64) ([]orbital.ContactWindow, error) {
+	args := m.Called(ctx, gsLat, gsLon, start, end, minElevDeg)
+	if ws, ok := args.Get(0).([]orbital.ContactWindow); ok {
+		return ws, args.Error(1)
+	}
+	return nil, args.Error(1)
+}
